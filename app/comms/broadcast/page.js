e13dfd293f36651a3ac73ad15012413c049d0a38
Kizito007 <nwakakelechi49gmail.com>
2024-11-26 19:10:55 +0100

    feat: email broadcast

--- app/comms/broadcast/page.js
+++ app/comms/broadcast/page.js
@@ -4,33 +4,24 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import FlashBanner from "@/app/components/common/FlashBanner";
 import LoadingSpinner from "@/app/components/common/LoadingSpinner";
-import { PhotoIcon } from '@heroicons/react/24/solid'
 import { useAuth, useFaceAuth } from "@/app/components/hooks/useAuth";
 import Navbar from "@/app/components/layout/Navbar";
 
 export default function CreateBroadcast() {
   useAuth();
   useFaceAuth('comms/broadcast');
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-    securityAnswer: "",
-    role: "EDITOR", // Default value
-    file: null,
-  });
+  const [subject, setSubject] = useState("");
+  const [text, setText] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(null);
 
-  const handleChange = (e) => {
-    const { name, value, files } = e.target;
-    if (name === "file") {
-      setFormData((prev) => ({ ...prev, file: files[0] }));
-    } else {
-      setFormData((prev) => ({ ...prev, [name]: value }));
-    }
+  const handleSubjectChange = (e) => {
+    setSubject(e.target.value);
+  };
+  const handleTextChange = (e) => {
+    setText(e.target.value);
   };
 
   const handleSubmit = async (e) => {
@@ -38,25 +29,20 @@ export default function CreateBroadcast() {
     setIsLoading(true);
 
     try {
-      const formDataToSubmit = new FormData();
-      for (const key in formData) {
-        formDataToSubmit.append(key, formData[key]);
-      }
       const jwt = localStorage.getItem('jwt')
-
       const apiUrl =
-        "https://school-project-backend-p17b.onrender.com/api/v1/commerce/admin/admin-mgmt/admins";
-      await axios.post(apiUrl, formDataToSubmit, {
-        headers: { 'Content-Type': 'multipart/form-data', 'Authorization': `Bearer ${jwt}` },
+        "https://school-project-backend-p17b.onrender.com/api/v1/commerce/admin/admin-mgmt/send-bulk-email";
+      await axios.post(apiUrl, {subject, text}, {
+        headers: { 'Authorization': `Bearer ${jwt}` },
       });
 
-      setSuccess("Admin created successfully!");
+      setSuccess("Emails broadcasted successfully!");
       setTimeout(() => {
-        router.push("/admins");
+        router.push("/comms");
       }, 2000);
     } catch (error) {
       setError(
-        error.response?.data.message || "An error occurred while creating the administrator."
+        error.response?.data.message || "An error occurred while broadcasting the emails."
       );
     } finally {
       setIsLoading(false);
@@ -107,25 +93,25 @@ export default function CreateBroadcast() {
                   name="subject"
                   type="subject"
                   required
-                  value={formData.password}
-                  onChange={handleChange}
+                  value={subject}
+                  onChange={handleSubjectChange}
                   className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm/6"
                 />
               </div>
             </div>
 
             <div className="col-span-full">
-                    <label htmlFor="description" className="block text-sm/6 font-medium text-gray-900">
-                      Description
+                    <label htmlFor="text" className="block text-sm/6 font-medium text-gray-900">
+                      Text
                     </label>
                     <div className="mt-2">
                       <textarea
-                        id="description"
-                        name="description"
+                        id="text"
+                        name="text"
                         rows={3}
                         className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm/6"
-                        value={formData.description}
-                        onChange={handleChange}
+                        value={text}
+                        onChange={handleTextChange}
                       />
                     </div>
                     <p className="mt-3 text-sm/6 text-gray-600">Say something nice to your users.</p>
